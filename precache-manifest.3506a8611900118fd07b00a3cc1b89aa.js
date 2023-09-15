f0ba76fb0b6f20d0797f89a235969fa38f85ac90
Code Sandbox <hello@codesandbox.io>
2023-09-15 13:05:02 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.3506a8611900118fd07b00a3cc1b89aa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e8472157b5842fc7e7a4bbfb816b1f",
+    "url": "https://ganeshkumarmodela.github.io/csb-93nf33/index.html"
+  },
+  {
+    "revision": "da19ef799d3acc3cf7a4",
+    "url": "https://ganeshkumarmodela.github.io/csb-93nf33/static/js/2.566b72c8.chunk.js"
+  },
+  {
+    "revision": "65633084fd42fba51aa1",
+    "url": "https://ganeshkumarmodela.github.io/csb-93nf33/static/js/main.ee76712b.chunk.js"
+  },
+  {
+    "revision": "87787571761161b0552e",
+    "url": "https://ganeshkumarmodela.github.io/csb-93nf33/static/js/runtime-main.b99ca821.js"
+  }
+]);
